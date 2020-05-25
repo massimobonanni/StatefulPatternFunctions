bf338df914fcd9571885f0551e61c5884bc1fbd3
Massimo Bonanni <massimo.bonanni@tiscali.it>
2020-05-25 10:52:31 +0200

    Add web, core and rest projects

---
+++ StatefulPatternFunctions.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StatefulPatternFunctions.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
