253af9e649f21fe44115f3438136fb6556ad6619
Massimo Bonanni <massimo.bonanni@tiscali.it>
2020-05-26 08:51:24 +0200

    Add certification details (action + view)

---
+++ StatefulPatternFunctions.Web/Controllers/CertificationsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StatefulPatternFunctions.Core.Interfaces;
+using StatefulPatternFunctions.Web.Models.Certifications;
+
+namespace StatefulPatternFunctions.Web.Controllers
+{
+    public class CertificationsController : Controller
+    {
+        private readonly ILogger<CertificationsController> _logger;
+        private readonly ICertificationProfilesProvider _certificationProfilesProvider;
+
+        public CertificationsController(ICertificationProfilesProvider certificationProfilesProvider,
+            ILogger<CertificationsController> logger)
+        {
+            if (certificationProfilesProvider == null)
+                throw new ArgumentNullException(nameof(certificationProfilesProvider));
+            if (logger == null)
+                throw new ArgumentNullException(nameof(logger));
+
+            this._certificationProfilesProvider = certificationProfilesProvider;
+            this._logger = logger;
+        }
+
+        public async Task<ActionResult> Details(Guid profileId, Guid certificationId)
+        {
+            var model = new DetailModel();
+            var profile = await this._certificationProfilesProvider.GetCertificationProfileAsync(profileId, default);
+            if (profile == null)
+                return RedirectToAction(nameof(Index));
+            var certification = profile.Certifications.FirstOrDefault(c => c.Id == certificationId);
+            if (certification == null)
+                return RedirectToAction(nameof(Index));
+
+            model.CertificationId = certification.Id;
+            model.CertificationName = certification.Name;
+            model.CredentialId = certification.CredentialId;
+            model.CredentialUrl = certification.CredentialUrl;
+            model.ExpirationDate = certification.ExpirationDate;
+            model.IssueDate = certification.IssueDate;
+            model.IssuingOrganization = certification.IssuingOrganization;
+            model.ProfileId = profile.Id;
+            model.LastName = profile.LastName;
+            model.FirstName = profile.FirstName;
+            model.Email = profile.Email;
+
+            return View(model);
+        }
+
+        // GET: CertificationsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CertificationsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CertificationsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CertificationsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CertificationsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CertificationsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ StatefulPatternFunctions.Web/Models/Certifications/DetailModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatefulPatternFunctions.Web.Models.Certifications
+{
+    public class DetailModel
+    {
+
+        public Guid ProfileId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public Guid CertificationId { get; set; }
+        
+        public string CertificationName { get; set; }
+
+        public string IssuingOrganization { get; set; }
+
+        public string CredentialUrl { get; set; }
+
+        public string CredentialId { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime IssueDate { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime? ExpirationDate { get; set; }
+    }
+}
